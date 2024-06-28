4c77044b53f920c370bb13b0afd9b0c1289820f5
Sova <108209320+NceSova@users.noreply.github.com>
2024-06-28 17:43:39 +0300

    added name, second name, phone number and email validation

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,107 @@
 import "./index.css";
 
+class FormElem {
+  elem: HTMLInputElement;
+  elemError: HTMLSpanElement;
+
+  constructor(elem: HTMLInputElement) {
+    this.elem = elem;
+    this.elemError = document.querySelector(
+      `.${this.elem.className} + span.error`
+    );
+    this.elem.addEventListener("input", (e) => {
+      if (this.isValid()) {
+        this.elemError.textContent = "";
+        this.elemError.className = "error";
+      } else {
+        this.showError();
+      }
+    });
+  }
+
+  showError(): void {}
+  isValid(): boolean {
+    return this.elem.validity.valid;
+  }
+}
+
+class SecondName extends FormElem {
+  constructor() {
+    super(document.querySelector(".second") as HTMLInputElement);
+  }
+  showError(): void {
+    const errorType = this.elem.validity;
+    if (errorType.valueMissing) {
+      this.elemError.textContent = "Вам нужно ввести свою фамилию.";
+    }
+    this.elemError.className = "error active";
+  }
+}
+
+class FirstName extends FormElem {
+  constructor() {
+    super(document.querySelector(".name") as HTMLInputElement);
+  }
+  showError(): void {
+    const errorType = this.elem.validity;
+    if (errorType.valueMissing) {
+      this.elemError.textContent = "Вам нужно ввести свое имя.";
+    }
+    this.elemError.className = "error active";
+  }
+}
+
+class PhoneNumber extends FormElem {
+  constructor() {
+    super(document.querySelector(".phone") as HTMLInputElement);
+  }
+  showError(): void {
+    const errorType = this.elem.validity;
+    if (errorType.valueMissing) {
+      this.elemError.textContent = "Вам нужно ввести свой номер телефона.";
+    } else if (errorType.patternMismatch) {
+      this.elemError.textContent = "Введите номер в формате: 81112223344";
+    }
+    this.elemError.className = "error active";
+  }
+}
+
+class EmailAddress extends FormElem {
+  constructor() {
+    super(document.querySelector(".email") as HTMLInputElement);
+  }
+  showError(): void {
+    const errorType = this.elem.validity;
+    if (errorType.valueMissing) {
+      this.elemError.textContent = "Вам нужно ввести свою электронную почту.";
+    } else if (errorType.patternMismatch) {
+      this.elemError.textContent = "Введите почту в формате: user@host.domen";
+    }
+    this.elemError.className = "error active";
+  }
+}
+
 const form = document.querySelector("form") as HTMLFormElement;
 
-const second = document.querySelector(".second") as HTMLInputElement;
-const secondError = document.querySelector(
-  ".second + span.error"
-) as HTMLSpanElement;
+const second = new SecondName();
+const first = new FirstName();
+const phone = new PhoneNumber();
+const email = new EmailAddress();
 
-const name = document.querySelector(".name") as HTMLInputElement;
-const nameError = document.querySelector(
-  ".name + span.error"
-) as HTMLSpanElement;
-
-const phone = document.querySelector(".phone") as HTMLInputElement;
-const phoneError = document.querySelector(
-  ".phone + span.error"
-) as HTMLSpanElement;
-
-const email = document.querySelector(".email") as HTMLInputElement;
-const emailError = document.querySelector(
-  ".email + span.error"
-) as HTMLSpanElement;
-
-const password = document.querySelector(".password") as HTMLInputElement;
-const passwordError = document.querySelector(
-  ".password + span.error"
-) as HTMLSpanElement;
-
-const repeat = document.querySelector(".repeat") as HTMLInputElement;
-const repeatError = document.querySelector(
-  ".repeat + span.error"
-) as HTMLSpanElement;
+form.addEventListener("submit", (event) => {
+  const inputs = [second, first, phone, email];
+  if (
+    !second.isValid() ||
+    !first.isValid() ||
+    !phone.isValid() ||
+    !email.isValid()
+  ) {
+    inputs.forEach((elem) => {
+      if (!elem.isValid()) {
+        elem.showError();
+        console.log("ds");
+      }
+    });
+    event.preventDefault();
+  }
+});
