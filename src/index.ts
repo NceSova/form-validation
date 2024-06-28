6c1e28d3e6b633a4a5125faf643657a139921663
Sova <108209320+NceSova@users.noreply.github.com>
2024-06-28 10:01:04 +0300

    trying to add phone validation error

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
 import "./index.css";
+
+const form = document.querySelector("form") as HTMLFormElement;
+const phone = document.querySelector(".phone") as HTMLInputElement;
+const phoneError = document.querySelector(
+  ".phone + span.error"
+) as HTMLSpanElement;
+
+phone.addEventListener("input", (e) => {
+  if (phone.validity.valid) {
+    phoneError.textContent = "";
+    phoneError.className = "error";
+  } else {
+    showPhoneError();
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  if (!phone.validity.valid) {
+    showPhoneError();
+    e.preventDefault();
+  }
+});
+
+function showPhoneError() {}
