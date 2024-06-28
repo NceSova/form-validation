fcfcc8d75b27af67e76c15f648b795caef6817bb
Sova <108209320+NceSova@users.noreply.github.com>
2024-06-28 15:33:18 +0300

    add all HTMLInputElements

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,33 @@
 import "./index.css";
 
 const form = document.querySelector("form") as HTMLFormElement;
+
+const second = document.querySelector(".second") as HTMLInputElement;
+const secondError = document.querySelector(
+  ".second + span.error"
+) as HTMLSpanElement;
+
+const name = document.querySelector(".name") as HTMLInputElement;
+const nameError = document.querySelector(
+  ".name + span.error"
+) as HTMLSpanElement;
+
 const phone = document.querySelector(".phone") as HTMLInputElement;
 const phoneError = document.querySelector(
   ".phone + span.error"
 ) as HTMLSpanElement;
 
-phone.addEventListener("input", (e) => {
-  if (phone.validity.valid) {
-    phoneError.textContent = "";
-    phoneError.className = "error";
-  } else {
-    showPhoneError();
-  }
-});
+const email = document.querySelector(".email") as HTMLInputElement;
+const emailError = document.querySelector(
+  ".email + span.error"
+) as HTMLSpanElement;
 
-form.addEventListener("submit", (e) => {
-  if (!phone.validity.valid) {
-    showPhoneError();
-    e.preventDefault();
-  }
-});
+const password = document.querySelector(".password") as HTMLInputElement;
+const passwordError = document.querySelector(
+  ".password + span.error"
+) as HTMLSpanElement;
 
-function showPhoneError() {}
+const repeat = document.querySelector(".repeat") as HTMLInputElement;
+const repeatError = document.querySelector(
+  ".repeat + span.error"
+) as HTMLSpanElement;
